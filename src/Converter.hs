e3a77305b4e3089d01937c77801bbe0051bf6455
Gabriel Adomnicai <gabesoft@gmail.com>
2016-09-18 23:37:01 -0700

    Converter fixes

--- src/Converter.hs
+++ src/Converter.hs
@@ -1,42 +1,50 @@
 -- | Extractor for feeds and posts
-module Converter (extractFeed, extractPosts, extractFeedAndPosts)
-       where
+module Converter
+  ( extractFeed
+  , extractPosts
+  , extractFeedAndPosts
+  ) where
 
-import           Control.Exception
-import qualified Converter.Atom       as A
-import qualified Converter.RSS        as R
-import           Data.ByteString.Lazy
-import           Text.XML
-import           Types
+import Control.Exception
+import qualified Converter.Atom as A
+import qualified Converter.RSS as R
+import Data.ByteString.Lazy
+import Text.XML
+import Types
+
+data UnsupportedFormat =
+  UnsupportedFormat
+  deriving (Eq, Ord, Show)
+
+instance Exception UnsupportedFormat
 
 -- |
 -- Extract feed meta-data and post entries from an XML byte-string
-extractFeedAndPosts
-  :: ByteString -> Either SomeException (Feed,[Post])
-extractFeedAndPosts bytes =
-  do doc <- parseLBS def bytes
-     return (extractFeed' doc,extractPosts' doc)
+extractFeedAndPosts :: ByteString -> Either SomeException (Feed, [Post])
+extractFeedAndPosts bytes = do
+  doc <- parseLBS def bytes
+  feed <- extractFeed' doc
+  posts <- extractPosts' doc
+  return (feed, posts)
 
 -- |
 -- Extract feed meta-data an XML byte-string
-extractFeed
-  :: ByteString -> Either SomeException Feed
-extractFeed bytes = parseLBS def bytes >>= Right . extractFeed'
+extractFeed :: ByteString -> Either SomeException Feed
+extractFeed bytes = parseLBS def bytes >>= extractFeed'
 
 -- |
 -- Extract post entries from an XML byte-string
-extractPosts
-  :: ByteString -> Either SomeException [Post]
-extractPosts bytes = parseLBS def bytes >>= Right . extractPosts'
+extractPosts :: ByteString -> Either SomeException [Post]
+extractPosts bytes = parseLBS def bytes >>= extractPosts'
 
-extractFeed' :: Document -> Feed
+extractFeed' :: Document -> Either SomeException Feed
 extractFeed' doc
-  | A.isAtom doc = A.extractFeed doc
-  | R.isRSS doc = R.extractFeed doc
-  | otherwise = error "unsupported format"
+  | A.isAtom doc = Right $ A.extractFeed doc
+  | R.isRSS doc = Right $ R.extractFeed doc
+  | otherwise = Left (toException UnsupportedFormat)
 
-extractPosts' :: Document -> [Post]
+extractPosts' :: Document -> Either SomeException [Post]
 extractPosts' doc
-  | A.isAtom doc = A.extractPosts doc
-  | R.isRSS doc = R.extractPosts doc
-  | otherwise = error "unsupported format"
+  | A.isAtom doc = Right $ A.extractPosts doc
+  | R.isRSS doc = Right $ R.extractPosts doc
+  | otherwise = Left (toException UnsupportedFormat)

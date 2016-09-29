13ba4cddbbacc2394adca0ec2a8913636ad93ecb
Gabriel Adomnicai <gadomnicai@linkedin.com>
2016-09-28 17:38:52 -0700

    New sanitizer implementation - in progress

--- src/SanitizerNew.hs
+++ src/SanitizerNew.hs
@@ -13,6 +13,7 @@ import qualified Data.Map as Map
 import Data.Maybe
 import Data.String
 import Data.Text (Text, pack, unpack)
+import qualified Data.Text as T
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Tree
 
@@ -33,36 +34,82 @@ import Text.HTML.TagSoup.Tree
 --                 [TagTree str]
 --     | TagLeaf (Tag str)
 --     deriving (Eq,Ord,Show)
-processTree :: Text -> Text
-processTree = renderTree . transformTree (toList . cleanTree) . parseTree
+processTree
+    :: Text -> Text
+processTree =
+    renderTree . modifyTree canProcess (toList . cleanTree) . parseTree
 
 cleanTree :: TagTree Text -> Maybe (TagTree Text)
-cleanTree = removeTag "script" >=> removeTag "style" >=> removeTag "iframe"
+cleanTree =
+    removeTag "script" >=>
+    removeTag "style" >=> removeTag "iframe" >=> removeWS >=> removeSLink
+
+modifyTree :: (TagTree s -> Bool)
+           -> (TagTree s -> [TagTree s])
+           -> [TagTree s]
+           -> [TagTree s]
+modifyTree descend act = concatMap f
+  where
+    f t@(TagBranch val attr sub)
+      | descend t = act $ TagBranch val attr (modifyTree descend act sub)
+      | otherwise = act t
+    f x = act x
 
 modifyTag
-  :: (Monoid a, Eq a)
-  => a -> (TagTree a -> Maybe (TagTree a)) -> TagTree a -> Maybe (TagTree a)
+    :: (Monoid a, Eq a)
+    => a -> (TagTree a -> Maybe (TagTree a)) -> TagTree a -> Maybe (TagTree a)
 modifyTag name f t =
-  if (t ^. ttname) == name
-    then f t
-    else Just t
+    if (t ^. ttname) == name -- TODO use case insensitive here
+        then f t
+        else Just t
+
+removeWS :: TagTree Text -> Maybe (TagTree Text)
+removeWS t =
+    if (t ^. ttname & remove & T.null)
+        then Nothing
+        else Just t
+  where
+    remove = T.unwords . T.words
 
 removeTag
-  :: (Monoid a, Eq a)
-  => a -> TagTree a -> Maybe (TagTree a)
+    :: (Monoid a, Eq a)
+    => a -> TagTree a -> Maybe (TagTree a)
 removeTag name = modifyTag name (const Nothing)
+
+removeSLink
+    :: (Monoid a, Eq a, IsString a)
+    => TagTree a -> Maybe (TagTree a)
+removeSLink t =
+    if link && stylesheet rel
+        then Nothing
+        else Just t
+  where
+    link = t ^. ttname == "link"
+    rel = t ^. ttattr & lookup "rel"
+    stylesheet (Just "stylesheet") = True -- TODO use case insensitive
+    stylesheet _ = False
 
 printTextTree :: Text -> IO ()
 printTextTree = putStr . unpack
+
+canProcess
+    :: (Monoid a, Eq a, IsString a)
+    => TagTree a -> Bool
+canProcess t = not (n == "pre" || n == "code")
+  where
+    n = t ^. ttname
 
 -- |
 -- Lens for a tree tag name
 -- > tag ^. ttname
 -- > tag & ttname .~ "div"
 ttname
-  :: (Functor f, Monoid a)
-  => (a -> f a) -> TagTree a -> f (TagTree a)
-ttname f (TagBranch name a t) = (\n -> TagBranch n a t) <$> f name
+    :: (Functor f, Monoid a)
+    => (a -> f a) -> TagTree a -> f (TagTree a)
+ttname f (TagBranch name a t) =
+    (\n ->
+          TagBranch n a t) <$>
+    f name
 ttname f (TagLeaf t) = TagLeaf <$> tname f t
 
 -- |
@@ -71,9 +118,12 @@ ttname f (TagLeaf t) = TagLeaf <$> tname f t
 -- > tag & ttattr %~ (sattr "href" $ const "/about.html")
 -- > tag ^. ttattr & lookup "href"
 ttattr
-  :: Functor f
-  => ([Attribute a] -> f [Attribute a]) -> TagTree a -> f (TagTree a)
-ttattr f (TagBranch n attr t) = (\a -> TagBranch n a t) <$> f attr
+    :: Functor f
+    => ([Attribute a] -> f [Attribute a]) -> TagTree a -> f (TagTree a)
+ttattr f (TagBranch n attr t) =
+    (\a ->
+          TagBranch n a t) <$>
+    f attr
 ttattr f (TagLeaf t) = TagLeaf <$> tattr f t
 
 -- |
@@ -81,8 +131,8 @@ ttattr f (TagLeaf t) = TagLeaf <$> tattr f t
 -- > tag ^. tname
 -- > tag & tname .~ "div"
 tname
-  :: (Functor f, Monoid a)
-  => (a -> f a) -> Tag a -> f (Tag a)
+    :: (Functor f, Monoid a)
+    => (a -> f a) -> Tag a -> f (Tag a)
 tname f (TagOpen name attrs) = flip TagOpen attrs <$> f name
 tname f (TagClose name) = TagClose <$> f name
 tname f (TagText name) = TagText <$> f name
@@ -96,21 +146,21 @@ tname f x@(TagPosition _ _) = x <$ void (f mempty)
 -- > tag & tattr %~ (sattr "href" $ const "/home.html")
 -- > tag ^. tattr & lookup "href"
 tattr
-  :: Functor f
-  => ([Attribute str] -> f [Attribute str]) -> Tag str -> f (Tag str)
+    :: Functor f
+    => ([Attribute str] -> f [Attribute str]) -> Tag str -> f (Tag str)
 tattr f (TagOpen name attr) = TagOpen name <$> f attr
 tattr f t = t <$ void (f mempty)
 
 -- |
 -- Set the value of an attribute (overwrite or insert)
 sattr
-  :: Ord a
-  => a -> a -> [Attribute a] -> [Attribute a]
+    :: Ord a
+    => a -> a -> [Attribute a] -> [Attribute a]
 sattr name value = Map.toList . (at name ?~ value) . Map.fromList
 
 -- |
 -- Modify the value of an attribute if it exists
 fattr
-  :: Ord a
-  => a -> (a -> a) -> [Attribute a] -> [Attribute a]
+    :: Ord a
+    => a -> (a -> a) -> [Attribute a] -> [Attribute a]
 fattr name f = Map.toList . (at name %~ fmap f) . Map.fromList

f8c6e39849a5d801ab01f8736a404fa800ca267a
Gabriel Adomnicai <gabesoft@gmail.com>
2018-01-29 20:06:04 -0800

    Upgrade to lts-10.4

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (intero-targets "ufeed:lib" "ufeed:exe:ufeed-server" "ufeed:exe:ufeed-updater" "ufeed:test:converter-atom" "ufeed:test:converter-rss" "ufeed:test:feed-reader" "ufeed:test:feed-updater" "ufeed:test:sanitizer")))
